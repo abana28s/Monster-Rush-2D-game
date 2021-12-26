0d2cbb2f93c102481eb255f4f9555ffcc41e9540
abanasidhu <abana2767@gmail.com>
2021-12-26 11:34:20 +0530

    Initial commit

---
+++ Assets/Scripts/GameplayUIcontroller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayUIcontroller : MonoBehaviour
+{
+    public void Restart(){
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void Home(){
+         SceneManager.LoadScene("MainMenu");
+    }
+}
